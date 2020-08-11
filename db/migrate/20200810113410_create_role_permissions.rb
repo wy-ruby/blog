81561e7fa3c47f39cea5ec58138934d89fefe999
王允 <906971957@qq.com>
2020-08-11 15:05:38 +0800

    修复role_permission表的多对多关联配置错误

--- db/migrate/20200810113410_create_role_permissions.rb
+++ db/migrate/20200810113410_create_role_permissions.rb
@@ -1,8 +1,8 @@
 class CreateRolePermissions < ActiveRecord::Migration[5.2]
   def change
     create_table :role_permissions do |t|
-      t.references :roles, comment: "多对多关联中的角色id"
-      t.references :permissions, comment: "多对多关联中的权限id"
+      t.references :role, comment: "多对多关联中的角色id"
+      t.references :permission, comment: "多对多关联中的权限id"
       t.timestamps
     end
   end
